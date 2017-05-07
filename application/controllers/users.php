9de02befa06770e784a56cc9e914b11b9a91890c
bear <lalabear0014@gmail.com>
2017-05-07 22:59:44 +0800

    Setting User

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,14 +1,60 @@
 <?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
     class Users extends CI_Controller {
-        public function showuser() {
-            echo "showuser";
-            $this->load->database();
-            var_dump($this->db);
+        public function __construst() {
+            parent::__construst();
+            $this->load->library('session');
+            $this->load->model('user_model', 'um');
+            $this->load->helper('date');
         }
+       
         public function index() {
-            $this->load->model("user_model");
-            $data["records"] = $this->user_model->getData();
-            $this->load->view("users/index", $data);
+            if (isset($_SESSION['user']) && ($_SESSION['user'] != "")) {
+                redirect(base_url('events/index'));
+            }
+            $this->load->view('users/login');
+        }
+
+        public function loginProcess() {
+            if (isset($_SESSION['user']) && ($_SESSION['user'] != "")) {
+                $this->session->unset_userdata('user');
+                $this->session->sess_destroy();
+            }
+
+            $Message = array();
+            $username = trim($_POST['username']);
+            $password = trim($_POST['password']);
+            $data = array(
+                'username' => $username,
+                'password' => $password,
+            );
+            if ($this->um->validate($data)) {
+                date_default_timezone_set("Asia/Taipei");
+                $sessionText = array(
+                    'user' => true,
+                    'ip_address' => $this->input->ip_address(),
+                    'user_agent' => $this->agent->agent_string(),
+                    'last_activity' => date("Y/m/d H:i:s"),
+                    'user_data' => $username,
+                );
+                $this->session->set_userdata($sessionText);
+
+                $sessionData = $this->session->all_userdata();
+                $this->um->logSession($sessionData);
+                redirect(base_url('events/index'));
+            } else {
+                $Message['username'] = $username;
+                $Message['errorMessage'] = "user account and password isn't correct";
+                $this->load->view('users/login', $Message);
+            }
+
+        }
+
+        public function logout() {
+            $this->session->unset_userdata('user');
+            $this->session->sess_destroy();
+            redirect(base_url('events/index'));
         }
     }
 ?>
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,8 +1,28 @@
 <?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
     class User_model extends CI_Model {
-        public function getData() {
-            $query = $this->db->get('usertable');
-            return $query->result();
+        public function __construct() {
+            parent::__construct();
         }
+
+        public function getData() {
+            $query = $this->db->get('users');
+            if ($query->num_rows() > 0) {
+                return $query->result();
+            }else {
+                return false;
+            }
+        }
+
+        public function validate($data) {
+            
+            $query = $this->db->get('users');
+        }
+
+        public function logSession($sessionData) {
+
+        }
+
     }
 ?>
